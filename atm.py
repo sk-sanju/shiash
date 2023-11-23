eac628742ae4ca23fd6f8d6a2ec0bfa91f92c7e3
sk-sanju <sanjayskpy1@gmail.com>
2023-11-23 21:44:16 +0530

    first commit

---
+++ atm.py
@@ -0,0 +1,37 @@
+balance = 0
+
+def deposit(amount):
+    global balance
+    balance += amount
+    print(f"Deposited ₹{amount}. New balance: ₹{balance}")
+
+def withdraw(amount):
+    global balance
+    if amount <= balance:
+        balance -= amount
+        print(f"Withdrew ₹{amount}. New balance: ₹{balance}")
+    else:
+        print("Insufficient funds!")
+
+def check_balance():
+    global balance
+    print(f"Your balance is: ₹{balance}")
+
+while True:
+    print("===SANJU-ATM===")
+    print("\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        amount = float(input("Enter the amount to deposit: "))
+        deposit(amount)
+    elif choice == '2':
+        amount = float(input("Enter the amount to withdraw: "))
+        withdraw(amount)
+    elif choice == '3':
+        check_balance()
+    elif choice == '4':
+        print("Thank you for using the ATM. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter a correct option.")
---
+++ readme.md
@@ -0,0 +1,5 @@
+This program is a simple Python program for an ATM that allows depositing, withdrawing, checking balance, and exiting.
+
+SCREEN SHOT THIS PROJECT
+
+<img width="378" alt="atm" src="https://github.com/sk-sanju/sk-sanju/assets/133774180/076e082a-5825-4429-87aa-d5a0ac0ce5f5">
