5c23280c65aa883bed262d6f25a2e44941216c6b
sk-sanju <sanjayskpy1@gmail.com>
2024-02-03 13:25:21 +0530

    add new project

--- encrypt_dcrypt/enc_dc/first_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import EncryptedFile
-
-class EncryptedFileForm(forms.ModelForm):
-    class Meta:
-        model = EncryptedFile
-        fields = ['file']
--- encrypt_dcrypt/enc_dc/first_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import upload_file, file_list
-
-urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
-    path('file-list/', file_list, name='file_list'),
-]
